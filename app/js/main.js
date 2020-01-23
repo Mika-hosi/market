946189311e3e9232f3ac86dd327854d2ce5dc54a
Mika-hosi <janekavaii@mail.ru>
2020-01-23 14:47:33 +0300

    footer added

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,25 @@
 $(function(){
+    $('.rate-star').rateYo({
+        rating: 5,
+        starWidth: "13px",
+        spacing: "5px",
+        readOnly: true
+    });
+
+    $('.featured__inner-box').slick({
+        dots: false,
+        prevArrow: $('.featured__arrow-left'),
+        nextArrow: $('.featured__arrow-right')
+    });
+
+    $('.feed__box').slick({
+        dots: false,
+        slidesToShow: 3,
+        prevArrow: $('.feed__arrow-left'),
+        nextArrow: $('.feed__arrow-right')
+    });
+
     let mixer = mixitup('.products__box');
+
+   
 });
